cf5e12a6297d1b792f34e3e7c31dcf3a02abfaf2
Going Bananaz <goingbananaz505@gmail.com>
2019-11-19 16:25:16 +0200

    Refactored isValidDayStartingFrom function for CodeForces' Round #600 problem #1253B, Silly Mistake

--- CFR600_1253B_Silly/solution.h
+++ CFR600_1253B_Silly/solution.h
@@ -4,9 +4,10 @@
 #include <vector>
 #include <unordered_set>
 
+
 class Solution {
 public:
-    std::vector<int> getEventsPartition(const std::vector<int>& events) {
+    std::vector<int> getEventsPartition(const std::vector<int>& events) const {
         std::vector<int> numberOfEvents;
         int startDayIndex = 0;
         while(startDayIndex < events.size()) {
@@ -25,50 +26,74 @@ public:
     
 private:
     bool isValidDayStartingFromIndex(const std::vector<int>& events, const int startIndex,
-                                     int& eventsCountToday) {
+                                     int& eventsCountToday) const {
         eventsCountToday = 0;
         
         int index = startIndex;
-        std::unordered_set<int> uniqueEmployeesIdsSeenToday;
-        std::unordered_set<int> employeesInTheOfficeAtThisTime;
+        EventValidatorState state;
+        bool isValidDayEnd = false;
         while(index < events.size()) {
-            int value = events[index];
+            int event = events[index];
             
-            bool isInEvent = (value > 0);
-            bool isOutEvent = (value < 0);
-            int employeeId = abs(value);
-
-            bool isEmployeeAlreadyIn = (uniqueEmployeesIdsSeenToday.find(employeeId) != uniqueEmployeesIdsSeenToday.end());
-            
-            if(isInEvent) {
-                if(isEmployeeAlreadyIn) {
-                    return false;
-                }
-                employeesInTheOfficeAtThisTime.insert(employeeId);
-                uniqueEmployeesIdsSeenToday.insert(employeeId);
-            } else if(isOutEvent) {
-                if(!isEmployeeAlreadyIn) {
-                    return false;
-                }
-                employeesInTheOfficeAtThisTime.erase(employeeId);
+            processEvent(event, state);
+            if(state.IsInvalidated) {
+                break;
             }
-            
-            bool isOfficeEmpty = (employeesInTheOfficeAtThisTime.size() == 0);
-            if(isOfficeEmpty) {
-                eventsCountToday = index - startIndex + 1;
-                return true;
+            if(state.IsOfficeEmpty()) {
+                isValidDayEnd = true;
+                break;
             }
             
             index++;
         }
         
-        bool isOfficeEmpty = (employeesInTheOfficeAtThisTime.size() == 0);
-        if(isOfficeEmpty) {
+        if(isValidDayEnd) {
             eventsCountToday = index - startIndex + 1;
             return true;
         }
         return false;
     }
+    
+    struct EventValidatorState;
+    void processEvent(const int event, EventValidatorState& state) const {
+        bool isInEvent = (event > 0);
+        bool isOutEvent = (event < 0);
+        int employeeId = abs(event);
+
+        bool isEmployeeAlreadyIn = state.IsEmployeeInOffice(employeeId);
+        
+        if(isInEvent) {
+            if(isEmployeeAlreadyIn) {
+                state.IsInvalidated = true;
+            }
+            state.ProcessInEvent(employeeId);
+        } else if(isOutEvent) {
+            if(!isEmployeeAlreadyIn) {
+                state.IsInvalidated = true;
+            }
+            state.ProcessOutEvent(employeeId);
+        }
+    }
+    
+    struct EventValidatorState {
+        std::unordered_set<int> uniqueEmployeesIdsSeenToday;
+        std::unordered_set<int> employeesInTheOfficeAtThisTime;
+        
+        bool IsOfficeEmpty() const {
+            return (employeesInTheOfficeAtThisTime.size() == 0);
+        }
+        bool IsEmployeeInOffice(int id) const {
+            return (uniqueEmployeesIdsSeenToday.find(id) != uniqueEmployeesIdsSeenToday.end());
+        }
+        void ProcessInEvent(int employeeId) {
+            employeesInTheOfficeAtThisTime.insert(employeeId);
+            uniqueEmployeesIdsSeenToday.insert(employeeId);
+        }
+        void ProcessOutEvent(int employeeId) {
+            employeesInTheOfficeAtThisTime.erase(employeeId);
+        }
+        bool IsInvalidated = false;
+    };
 };
 
 #endif
