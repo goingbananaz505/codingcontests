62add1e417d12a4b82363a99bf7e10142ee8efff
Going Bananaz <goingbananaz505@gmail.com>
2019-11-26 14:48:25 +0200

    Added source code for CodeForces' Round #602 problem #1262B, Messy

---
+++ CFR602_1262C_Messy/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include "consoleutils.h"
+#include "solution.h"
+
+using namespace std;
+namespace cu = consoleutils;
+
+Solution solution;
+int main() {
+    cu::runOverMultipleTestsInput([](const int testId) {
+        cu::read<int>();
+        int k = cu::read<int>();
+        string s = cu::read<string>();
+        
+        vector<pair<int, int>> pairs = solution.getPairs(s, k);
+        
+        cout << pairs.size() << endl;
+        for(const auto& pair : pairs) {
+            cout << pair.first << " " << pair.second << endl;
+        }
+    });
+    return 0;
+}
---
+++ CFR602_1262C_Messy/solution.h
@@ -0,0 +1,57 @@
+#ifndef solution_h
+#define solution_h
+
+#include <vector>
+#include <string>
+#include <utility>
+
+class Solution {
+public:
+    std::vector<std::pair<int, int>> getPairs(std::string s, const int k) const {
+        std::vector<std::pair<int, int>> result;
+        
+        for(int i = 0; i < 2 * (k - 1); i += 2) {
+            char first = s[i];
+            char second = s[i + 1];
+            if(first == ')') {
+                int index = findIndex(s, i, '(');
+                std::reverse(s.begin() + i, s.begin() + index + 1);
+                result.emplace_back(std::make_pair(indexToOrder(i), indexToOrder(index)));
+                second = s[i + 1];
+            }
+            if(second == '(') {
+                int index = findIndex(s, i, ')');
+                std::reverse(s.begin() + i + 1, s.begin() + index + 1);
+                result.emplace_back(std::make_pair(indexToOrder(i + 1), indexToOrder(index)));
+            }
+        }
+        
+        int end = 2 * (k - 1) + ((int)s.size() - 2 * (k - 1)) / 2;
+        for(int i = 2 * (k - 1); i < end; i++) {
+            char c = s[i];
+            if(c == ')') {
+                int index = findIndex(s, i, '(');
+                std::reverse(s.begin() + i, s.begin() + index + 1);
+                result.emplace_back(std::make_pair(i + 1, index + 1));
+            }
+        }
+        
+        return result;
+    }
+private:
+    inline int indexToOrder(int index) const {
+        return index + 1;
+    }
+    
+    int findIndex(const std::string& s, int start, char toFind) const {
+        for(int i = start; i < s.size(); i++) {
+            char c = s[i];
+            if(c == toFind) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+#endif
